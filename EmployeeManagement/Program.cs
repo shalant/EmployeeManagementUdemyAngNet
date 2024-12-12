adb97e2bd3e4e990f8b316df88e37b9ce8871906
shalant <doug.rosenberg@gmail.com>
2024-12-12 11:06:47 -0600

    finished module 53

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using EmployeeManagement.Data;
+
 namespace EmployeeManagement
 {
     public class Program
@@ -5,9 +9,25 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseInMemoryDatabase("EmployeeDb"));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("https://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+
+            app.UseCors("MyCors");
 
             app.Run();
         }
---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
